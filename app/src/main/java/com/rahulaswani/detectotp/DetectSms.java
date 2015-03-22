c66dfcd82a32af7c869591c1fa5e8317c9be3af8
Rahul Aswani <aswani@olacabs.com>
2015-03-22 20:55:41 +0530

    view change on successful OTP verification

--- app/src/main/java/com/rahulaswani/detectotp/DetectSms.java
+++ app/src/main/java/com/rahulaswani/detectotp/DetectSms.java
@@ -3,27 +3,23 @@ package com.rahulaswani.detectotp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.telephony.*;
-import android.util.Log;
 import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
-import java.util.regex.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class DetectSms extends BroadcastReceiver {
 
-    private String mMessage;
     private String OTPcode;
     private static final String TAG = "DetectSMS";
-    private static final String verification =  "code for Olacabs is ";
-
-    // Get the object of SmsManager
-    final SmsManager smsmgr = SmsManager.getDefault();
 
     @Override
     public void onReceive(Context context, Intent intent)
     {
-
         // Retrieves a map of extended data from the intent.
         final Bundle bundle = intent.getExtras();
 
@@ -37,13 +33,16 @@ public class DetectSms extends BroadcastReceiver {
 
                     SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
                     String phoneNumber = currentMessage.getDisplayOriginatingAddress();
-
+                    String sendername = currentMessage.getOriginatingAddress();
+                    Log.d(TAG,"sendername is "+sendername);
+                    String servicecntradd = currentMessage.getServiceCenterAddress();
+                    Log.d(TAG,"servicecenteraddress is : "+servicecntradd);
                     String senderNum = phoneNumber;
+                    Log.d(TAG,"Displayoriginationg address is : "+sendername);
                     String message = currentMessage.getDisplayMessageBody();
 
-                    Log.i(TAG, "senderNum: " + senderNum + "; message: " + message);
+                    Log.d(TAG, "senderNum: " + senderNum + "; message: " + message);
 
-                    int duration = Toast.LENGTH_LONG;
                     //if (senderNum.equalsIgnoreCase("Parthiban Chennai") || senderNum.equalsIgnoreCase("AD-bytwoo"))
                     //{
                         if (!message.isEmpty()) {
@@ -62,82 +61,16 @@ public class DetectSms extends BroadcastReceiver {
                             context.sendBroadcast(intentNew);
 
                             System.out.println(OTPcode);
-                            /*if (message.lastIndexOf(verification) > 0) {
-
-
-                                if (!message.isEmpty()) {
-                                    int index = message.lastIndexOf(verification);
-                                    int veri = verification.length();
-                                    System.out.println("length of verification is : " + veri);
-                                    System.out.println("Total length of string is : " + message.length());
-                                    System.out.println("index is :" + index);
-                                    int pickup = index + veri;
-                                    if (index > 0) {
-                                        Log.d(TAG, "OTP found");
-                                        String code = message.substring(pickup, pickup + 4)
-                                        Toast toast = Toast.makeText(context, "senderNum: " + senderNum + " OTP is " + code, duration).show();
-
-
-                                    } else
-                                        Log.d(TAG, "not found");
-                                }
-                            }*/
                         }
-
-
                    /* }
-                    else
-                    {
-                        Toast.makeText(context,"didn't identified the number", Toast.LENGTH_LONG).show();
-                    }*/
+                    else { Toast.makeText(context,"didn't identified the number", Toast.LENGTH_LONG).show(); }*/
                 }// end for loop
             } // bundle is null
 
         } catch (Exception e) {
             Log.e("SmsReceiver", "Exception smsReceiver" +e);
-
         }
-
     }
-
-
-
-    /*@Override
-    public void onReceive(Context context, Intent intent) {
-
-
-
-        // Retrieves a map of extended data from the intent.
-        final Bundle bundle = intent.getExtras();
-
-        try {
-
-            if (bundle != null) {
-
-                final Object[] pdusObj = (Object[]) bundle.get("pdus");
-
-                for (int i = 0; i < pdusObj.length; i++) {
-
-                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
-                    String phoneNumber = currentMessage.getDisplayOriginatingAddress();
-                    String senderNum = phoneNumber;
-                    mMessage = currentMessage.getDisplayMessageBody();
-                    //mMessage = mMessage.replace(AppConstants.SMS_VERIFY_FORMAT,"");
-                    Log.d(TAG, "SmsReceiver : senderNum: " + senderNum + "; message: " + mMessage);
-
-                   // ((YeloApplication) context.getApplicationContext()).getBus().post(new SmsVerification(mMessage));
-                    Log.d(TAG, "SmsReceiver : senderNum: " + senderNum + "; message: " + mMessage);
-
-
-
-                } // end for loop
-            } // bundle is null
-
-        } catch (Exception e) {
-            Log.e(TAG, "Exception smsReceiver" + e);
-
-        }
-    }*/
 }
 
 
--- app/src/main/java/com/rahulaswani/detectotp/MainActivity.java
+++ app/src/main/java/com/rahulaswani/detectotp/MainActivity.java
@@ -6,29 +6,41 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
-    private String mMessage;
-    private String OTPcode;
-
+    private String otp;
     TextView rcvdOtp;
-
-    String number = "0";
+    LinearLayout otpscreen;
+    LinearLayout succscreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rcvdOtp = (TextView)findViewById(R.id.textView_rcvdOTP);
+        otpscreen = (LinearLayout) findViewById(R.id.linearlayout_otpscreen);
+        succscreen = (LinearLayout) findViewById(R.id.linearlayout_successcreen);
 
-        IntentFilter filter = new IntentFilter();
+        rcvdOtp = (TextView) otpscreen.findViewById(R.id.textView_rcvdOTP);
+
+
+        /*IntentFilter filter = new IntentFilter();
         filter.addAction("SMS_RECEIVED");
         registerReceiver(broadcastReceiver, filter);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        filter.setPriority(2147483647);     set highest priority dynamically
+        receiver = new SmsAnalizer();
+        ac.registerReceiver(receiver, filter);*/
 
        /* Button sendotp = (Button) findViewById(R.id.button_sendotp);
         EditText getno = (EditText) findViewById(R.id.editText_takeno);
@@ -37,49 +49,77 @@ public class MainActivity extends Activity {
         sendotp.setOnClickListener(this);*/
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("SMS_RECEIVED");
+        registerReceiver(broadcastReceiver, filter);
+    }
+
     BroadcastReceiver broadcastReceiver = new DetectSms() {
         @Override
-        public void onReceive(Context context, Intent intent) {
+        public void onReceive(Context context, Intent intent)
+        {
+            final Bundle bundle = intent.getExtras();
 
-
-                final Bundle bundle = intent.getExtras();
-
-                rcvdOtp.setText("OTP is " + bundle.getString("otp_code"));
-
-
-
-
+                rcvdOtp.setText(bundle.getString("otp_code"));
+                otp = rcvdOtp.getText().toString().trim();
+                Log.d("DetectSMS",rcvdOtp.getText().toString().trim());
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    verifyOTP(otp);
+                }
+            },2500);
 
         }
     };
 
-    @Override
-    protected void onPause() {
-        unregisterReceiver(broadcastReceiver);
-        super.onPause();
+    public void verifyOTP(String otp)
+    {
+        this.otp = otp;
+        Log.d("DetectSMS","otp is : "+otp);
+
+        if(otp.equalsIgnoreCase("9880"))
+        {
+            otpscreen.setVisibility(View.GONE);
+            succscreen.setVisibility(View.VISIBLE);
+        }else
+        {
+            Toast.makeText(getApplicationContext(),"wrong OTP ",Toast.LENGTH_LONG).show();
+        }
     }
 
-    /*@Override
-    protected void onResume() {
-        this.broadcastReceiver = new DetectSms();
-        registerReceiver(this.broadcastReceiver,new IntentFilter(DetectSms.SMS_RECEIVED);
-
-        super.onResume();
-    }*/
 
     @Override
+    protected void onStop()
+    {
+        super.onStop();
+        try {
+
+            unregisterReceiver(broadcastReceiver);
+        }catch (IllegalArgumentException e)
+        {
+            Log.d("rahul","exception in onstop is : "+e.toString());
+        }
+    }
+
+   /* @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(broadcastReceiver);
-    }
+        try {
 
-    @Override
-    protected void onResume() {
-        IntentFilter filter = new IntentFilter();
-        filter.addAction("SMS_RECEIVED");
-        registerReceiver(broadcastReceiver, filter);
-        super.onResume();
-    }
+            unregisterReceiver(broadcastReceiver);
+        }catch (IllegalArgumentException e)
+        {
+            Log.d("rahul","exception in destroy is : "+e.toString());
+        }
+
+    }*/
 }
    /* @Override
     public void onClick(View v)
